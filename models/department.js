6fb6482fc1f1532061c4cf3161de7cdb327791f3
Carl Rosengren <carl.rosengren1@hotmail.com>
2023-06-28 13:20:27 -0700

    Department save validation and virtual user, city

--- models/department.js
+++ models/department.js
@@ -1,17 +1,32 @@
 const mongoose = require("mongoose");
+const {Company, Status: CompanyStatus} = require("./company");
 
 const Schema = mongoose.Schema;
 
+const Status = {
+  ACTIVE: 'active',
+  CLOSED: 'closed'
+};
+
 const DepartmentSchema = new Schema({
   company: { type: Schema.Types.ObjectId, ref: "Company", required: true},
-  geolocation: { type: Schema.Types.ObjectId, ref: "Geolocation"},
+  geolocation: { type: [Schema.Types.ObjectId], ref: "Geolocation"},
   name: { type: String, required: true },
   status: {
     type: String,
     required: true,
-    enum: ["Active", "Closed"],
-    default: "Active",
+    enum: [Status.ACTIVE, Status.CLOSED],
+    default: Status.ACTIVE,
   }
+});
+
+// Validate company ref
+DepartmentSchema.pre('save', async function(next) {
+  const company = await Company.findOne({ _id: this.company });
+  if (company.status === CompanyStatus.DECOMISSIONED) {
+    next(new Error("Company must not be decomissioned."));
+  }
+  next()
 });
 
 // Virtual for Department's URL
@@ -20,5 +35,19 @@ DepartmentSchema.virtual("url").get(function () {
   return `/catalog/Department/${this._id}`;
 });
 
+DepartmentSchema.virtual('cities', {
+  ref: 'Geolocation',
+  localField: 'geolocation',
+  foreignField: '_id',
+  options: { select: 'city' }
+});
+
+DepartmentSchema.virtual('users', {
+  ref: 'User',
+  localField: '_id',
+  foreignField: 'department'
+});
+
+
 // Export model
-module.exports = mongoose.model("Department", DepartmentSchema);
+module.exports = {Department: mongoose.model("Department", DepartmentSchema), Status};
