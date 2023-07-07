5162ba4dcb6d01763f226116400d0013d2451152
Carl Rosengren <carl.rosengren1@hotmail.com>
2023-07-07 14:11:24 -0700

    index route, controller and basic layout

---
+++ controllers/indexController.js
@@ -0,0 +1,32 @@
+const {Company, Status: CompanyStatus} = require("../models/company");
+const User = require("../models/user");
+const {Department, Status: DepartmentStatus} = require("../models/department");
+const asyncHandler = require("express-async-handler");
+
+
+exports.index = asyncHandler(async (req, res) => {
+    const [
+        numCompanies,
+        numDepartments,
+        numUsers,
+        usersWithoutDepartment,
+        departmentsWithDecomissionedCompany
+    ] = await Promise.all([
+        Company.countDocuments({}).exec(),
+        Department.countDocuments({}).exec(),
+        User.countDocuments({}).exec(),
+        User.find({department: null}).exec(),
+        Company.find({status:CompanyStatus.DECOMISSIONED}).populate("departments").exec(),
+    ]);
+
+    console.log(departmentsWithDecomissionedCompany)
+    res.render("index", {
+        title: "Org management portal",
+        company_count: numCompanies,
+        department_count: numDepartments,
+        user_count: numUsers,
+        users_without_departments: usersWithoutDepartment,
+        departments_with_decomissioned_company: departmentsWithDecomissionedCompany
+    });
+
+});
