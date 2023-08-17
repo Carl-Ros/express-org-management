2bb2ad07edf24ed1c9d4cb54403a5f7c960a2567
Carl Rosengren <carl.rosengren1@hotmail.com>
2023-08-17 13:13:46 -0700

    moved user treeview to index

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,8 @@
 const {Company, Status: CompanyStatus} = require("../models/company");
 const User = require("../models/user");
-const {Department, Status: DepartmentStatus} = require("../models/department");
+const {Department} = require("../models/department");
 const asyncHandler = require("express-async-handler");
+const { Tree } = require("../tree");
 
 
 exports.index = asyncHandler(async (req, res) => {
@@ -19,9 +20,17 @@ exports.index = asyncHandler(async (req, res) => {
         Company.find({status:CompanyStatus.DECOMISSIONED}).populate("departments").exec(),
     ]);
 
+    const users = await User.find({}).populate(["directReports", "manager"]).exec();
+    users.sort((a, b) => {
+        return a.surname.localeCompare(b.surname, "sv");
+    });
+    const userTree = new Tree(users, "directReports", "manager", "_id", "fullName");
+    const userTreeNodes = userTree.getNodes();
+
     console.log(departmentsWithDecomissionedCompany)
     res.render("index", {
         title: "Org management portal",
+        userTreeNodes,
         company_count: numCompanies,
         department_count: numDepartments,
         user_count: numUsers,
