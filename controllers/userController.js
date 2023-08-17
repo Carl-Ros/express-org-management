b2ccf51e8817be230ea3f55bc3e98ba5dfa85746
Carl Rosengren <carl.rosengren1@hotmail.com>
2023-08-17 13:12:43 -0700

    user search functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,93 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/user");
-const {Tree} = require("../tree");
+const { Company, Status: CompanyStatus } = require("../models/company")
 
-// Display detail page for a specific user.
 exports.user_get = asyncHandler(async (req, res) => {
     const user = await User.findById(req.params.id).populate(["department", "directReports", "manager"]); // add geolocation
     console.log(user)
-    res.render("user_get", { title: user.fullName, user: user, company: await user.company});
+    res.render("user_get", { title: user.fullName, user: user, company: await user.company });
+});
+
+exports.user_search = asyncHandler(async (req, res) => {
+    const nameParts = req.query.q?.split(' ') || [];
+
+    const selectedCategories = req.query.categories && JSON.parse(req.query.categories);
+
+    const query = {};
+
+    if(selectedCategories?.department) {
+        query.department = { $in: selectedCategories.department};
+    }
+
+    if(selectedCategories?.company.length > 0) {
+        query.company = { $in: selectedCategories.company};
+    }
+
+    const nameQuery = async (givenName, surname, op = 'and') => {
+        if(!givenName && !surname){
+            return User.find(query).populate(["directReports"]);
+        }
+
+        const _query = { ...query };
+        if(op === 'or'){
+            _query.$or = [
+                givenName && { givenName: { $regex: givenName, $options: 'i' } },
+                surname && { surname: { $regex: surname, $options: 'i' } },
+            ].filter(Boolean);
+        } else if (op === 'and'){
+            _query.$and = [
+                { givenName: { $regex: givenName, $options: 'i' } },
+                { surname: { $regex: surname, $options: 'i' } },
+            ];
+        }
+        return User.find(_query).populate(["directReports"]);
+    }
+
+    try {
+        let result = [];
+        if(nameParts.length > 1){
+            result = await nameQuery(nameParts.slice(0, nameParts.length - 1).join(' '), nameParts[nameParts.length - 1]);
+            
+            // givenName, cover remaining combinations
+            if(result.length === 0){
+                for(let i = 2; i < nameParts.length; i++){
+                    result = await nameQuery(nameParts.slice(0, i).join(' '), undefined, 'or');
+                    if(result.length > 0){
+                        break;
+                    }
+                }
+            }
+            
+            // surname, cover remaining combinations
+            if(result.length === 0){
+                for(let i = 2; i < nameParts.length; i++){
+                    result = await nameQuery(undefined, nameParts.slice(0, i).join(' '), 'or');
+                    if(result.length > 0){
+                        break;
+                    }
+                }
+            }
+        } else {
+            result = await nameQuery(nameParts[0], nameParts[0], 'or');
+        }
+        
+        if(selectedCategories?.isManager){
+            result = result.filter(({ directReports }) => directReports.length > 0 );
+        }
+
+        const jsonResponse = result.map(user => {
+            const fullName = user.fullName;
+            const userObject = user.toObject();
+            userObject.fullName = fullName;
+            return userObject;
+        })
+
+        console.log(jsonResponse)
+        res.json(jsonResponse);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'An error occurred while searching for users.' });
+    }
 });
 
 // Display user create form on GET.
@@ -40,14 +121,8 @@ exports.user_update_post = asyncHandler(async (req, res) => {
 });
 
 exports.user_list = asyncHandler(async (req, res) => {
-    const users = await User.find({}).populate(["directReports", "manager"]).exec();
-    users.sort((a, b) => {
-        return a.surname.localeCompare(b.surname, "sv");
-    })
-    const userTree = new Tree(users, "directReports", "manager", "_id", "fullName");
-    const treeNodes = userTree.getNodes();
-
+    const companies = await Company.find({ status: { $ne: CompanyStatus.DECOMISSIONED } }).exec();
     res.render("user_list", {
-        title: "Users", treeNodes
+        title: "Users", companies
     });
 });
